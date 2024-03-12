21df6ba2f8d78eb929f08ce3230b1bc1ccf6e8e5
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-12 14:29:37 -0300

    Create 1-apresentacao

---
+++ 1-apresentacao.py
@@ -0,0 +1,18 @@
+#teorias
+#   argumentações
+#       disputas__[
+#           Filosóficas - problemas -> Respostas
+#       disputas__]
+#   argumentações
+#teorias
+#
+#ideias
+#   teorias
+#       pesquisas
+#           Científicas - respostas -> Problemas
+#       pesquisas
+#   teorias
+#ideias
+#
+#
+####### - analogicamente, na pesquisa filosófica as teorias filosóficas são tentativas de solução dos problemas filosóficos.
